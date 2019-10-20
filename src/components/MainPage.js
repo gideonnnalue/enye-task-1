be70c65709951cb28cf9a70f7d4c925792f62228
Gideon Nnalue <emekannalue@gmail.com>
2019-10-19 20:43:46 -0400

    fixed css bug

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -25,6 +25,12 @@ class MainPage extends Component {
           <h1 style={{ fontWeight: "bold", color: "#FFF" }}>USER LIST</h1>
           <Content className="content">
             <Row style={{ width: "100%", height: "100%" }}>
+              {/* <Col className="section table__form">
+                <FormPage addUser={data => this.addUser(data)} />
+              </Col>
+              <Col className="section table__content">
+                <TablePage users={this.state.users} />
+              </Col> */}
               <Col md={24} lg={10} xl={10} className="section table__form">
                 <FormPage addUser={data => this.addUser(data)} />
               </Col>
