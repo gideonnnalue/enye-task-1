76a333045763f1619f28b899e2e746c1d7c972f0
Gideon Nnalue <emekannalue@gmail.com>
2019-10-21 09:02:31 -0400

    added redux and hooks

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -1,149 +1,155 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Form, Icon, Input, Button, Alert, DatePicker } from "antd";
 import moment from "moment";
+import { addUser } from "../actions/index";
 
-class FormPage extends Component {
-  state = {
+const FormPage = props => {
+  const [type, setType] = useState("error");
+  const [showMsg, setShowMsg] = useState(false);
+  const [formData, setFormData] = useState({
     firstname: "",
     lastname: "",
-    date: "1999-01-01",
+    date: moment().format("YYYY-MM-DD"),
     age: "",
-    hobby: "",
-    type: "error",
-    showMsg: false
-  };
-  handleSubmit = e => {
+    hobby: ""
+  });
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
     e.preventDefault();
-    const user = { ...this.state };
+    const user = { ...formData };
     user.key = Date.now();
-    delete user.type;
-    delete user.showMsg;
 
     let err = false;
     Object.keys(user).forEach((key, i) => {
-      if (this.state[key] === "") {
+      if (user[key] === "") {
         err = true;
       }
     });
     if (!err) {
-      this.setState({ type: "success", showMsg: true });
-      this.clearAlert();
-      this.props.addUser(user);
-      Object.keys(this.state).forEach((key, i) => {
-        if (key !== "date") {
-          this.setState({ [key]: "" });
-        }
+      setType("success");
+      setShowMsg(true);
+      clearAlert();
+      dispatch(addUser(user));
+
+      setFormData({
+        firstname: "",
+        lastname: "",
+        date: moment().format("YYYY-MM-DD"),
+        age: "",
+        hobby: ""
       });
     } else {
-      this.setState({ type: "error", showMsg: true });
-      this.clearAlert();
+      setType("error");
+      setShowMsg(true);
+      clearAlert();
     }
   };
 
-  clearAlert() {
+  const clearAlert = () => {
     setTimeout(() => {
-      this.setState({ showMsg: false });
+      setShowMsg(false);
     }, 1500);
-  }
+  };
 
-  componentDidMount() {
-    // console.log(moment().format('YYYY-MM-DD'))
-    this.setState({ date: moment().format("YYYY-MM-DD") });
-  }
+  const onChange = e => {
+    const current = { ...formData };
+    current[e.target.name] = e.target.value;
+    setFormData(current);
+  };
 
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+  const onDateChanged = (date, dateString) => {
+    const current = { ...formData };
+    current["date"] = dateString;
+    setFormData(current);
+  };
 
-  onDateChanged(date, dateString) {
-    this.setState({ date: dateString });
-  }
-  render() {
-    return (
-      <div className="form-section">
-        {this.state.showMsg ? (
-          <Alert
-            message={
-              this.state.type === "success"
-                ? "User added successfully"
-                : "Please fill in all fields..."
-            }
-            type={this.state.type}
-            style={{ marginBottom: "15px" }}
+  return (
+    <div className="form-section">
+      {showMsg ? (
+        <Alert
+          message={
+            type === "success"
+              ? "User added successfully"
+              : "Please fill in all fields..."
+          }
+          type={type}
+          style={{ marginBottom: "15px" }}
+        />
+      ) : null}
+      <h2>Fill in the data</h2>
+      <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item label="First Name">
+          <Input
+            placeholder="First Name"
+            value={formData.firstname}
+            onChange={e => onChange(e)}
+            name="firstname"
+            className="login-input"
           />
-        ) : null}
-        <h2>Fill in the data</h2>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item label="First Name">
-            <Input
-              placeholder="First Name"
-              value={this.state.firstname}
-              onChange={e => this.onChange(e)}
-              name="firstname"
-              className="login-input"
-            />
-          </Form.Item>
-          <Form.Item label="Last Name">
-            <Input
-              placeholder="Last Name"
-              value={this.state.lastname}
-              onChange={e => this.onChange(e)}
-              name="lastname"
-              className="login-input"
-            />
-          </Form.Item>
-          <Form.Item label="Date of Birth">
-            <DatePicker
-              defaultValue={moment("1999-01-01", "YYYY-MM-DD")}
-              placeholder="Select date"
-              value={moment(this.state.date)}
-              onChange={this.onDateChanged.bind(this)}
-              name="date"
-              className="login-input"
-            />
-          </Form.Item>
-          <Form.Item label="Age">
-            <Input
-              placeholder="Age"
-              value={this.state.age}
-              onChange={e => this.onChange(e)}
-              name="age"
-              type="number"
-              className="login-input"
-            />
-          </Form.Item>
-          <Form.Item label="Hobby">
-            <Input
-              placeholder="Hobby"
-              value={this.state.hobby}
-              onChange={e => this.onChange(e)}
-              name="hobby"
-              className="login-input"
-            />
-          </Form.Item>
-        </Form>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "30px"
-          }}
+        </Form.Item>
+        <Form.Item label="Last Name">
+          <Input
+            placeholder="Last Name"
+            value={formData.lastname}
+            onChange={e => onChange(e)}
+            name="lastname"
+            className="login-input"
+          />
+        </Form.Item>
+        <Form.Item label="Date of Birth">
+          <DatePicker
+            defaultValue={moment("1999-01-01", "YYYY-MM-DD")}
+            placeholder="Select date"
+            value={moment(formData.date)}
+            onChange={onDateChanged.bind(this)}
+            name="date"
+            className="login-input"
+          />
+        </Form.Item>
+        <Form.Item label="Age">
+          <Input
+            placeholder="Age"
+            value={formData.age}
+            onChange={e => onChange(e)}
+            name="age"
+            type="number"
+            className="login-input"
+          />
+        </Form.Item>
+        <Form.Item label="Hobby">
+          <Input
+            placeholder="Hobby"
+            value={formData.hobby}
+            onChange={e => onChange(e)}
+            name="hobby"
+            className="login-input"
+          />
+        </Form.Item>
+      </Form>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "30px"
+        }}
+      >
+        <Button
+          type="primary"
+          shape="round"
+          size={"large"}
+          className="submit-button"
+          htmlType="submit"
+          onClick={handleSubmit}
         >
-          <Button
-            type="primary"
-            shape="round"
-            size={"large"}
-            className="submit-button"
-            htmlType="submit"
-            onClick={this.handleSubmit}
-          >
-            save
-            <Icon type="right" />
-          </Button>
-        </div>
+          save
+          <Icon type="right" />
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default FormPage;
